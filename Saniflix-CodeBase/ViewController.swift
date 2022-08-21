13d96fbb1d839e7a5f070002bb8dd74cce420fdb
dotori-0 <sesan131@gmail.com>
2022-08-21 23:03:11 +0900

    Initial Commit

---
+++ Saniflix-CodeBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Saniflix-CodeBase
+//
+//  Created by SC on 2022/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
